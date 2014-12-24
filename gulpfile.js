e0caddcf9526427737a62a6e1aca40528b4ff3bc
EvAlex <evalexhimself@gmail.com>
2014-12-25 01:02:37 +0300

    package.json, gulp javascript task to uglify, browserify and source map generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,40 @@
+'use strict';
+
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
     // place code for your default task here
 });
+
+gulp.task('javascript', function () {
+
+    var bundler = browserify({
+        entries: ['./web/js/main.js'],
+        debug: true
+    });
+
+    var bundle = function () {
+        return bundler
+          .bundle()
+          .pipe(source(getBundleName() + '.js'))
+          .pipe(buffer())
+          .pipe(sourcemaps.init({ loadMaps: true }))
+            // Add transformation tasks to the pipeline here.
+            .pipe(uglify())
+          .pipe(sourcemaps.write('./'))
+          .pipe(gulp.dest('./dist/js/'));
+    };
+
+    return bundle();
+});
+
+var getBundleName = function () {
+    var version = require('./package.json').version;
+    var name = require('./package.json').name;
+    return version + '.' + name + '.' + 'min';
+};
--- web/js/main.js
+++ web/js/main.js
@@ -1,6 +1,12 @@
-define(['draw'], function(draw) {
-    return function() {
-        console.log('Yo!');
-        draw();
+requirejs.config({
+    baseUrl: 'js/',
+    urlArgs: 'bust=' + (new Date()).getTime(),
+    paths: {
+        'libs/d3': "libs/d3.min",
     }
+});
+
+require(['draw'], function (draw) {
+    console.log('Yo!');
+    draw();
 });
