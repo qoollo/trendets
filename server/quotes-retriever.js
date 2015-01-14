1431942274f5abcb37363bff098167caac60b7dd
EvAlex <evalexhimself@gmail.com>
2015-01-14 15:33:20 +0300

    oil quotes request added

--- server/quotes-retriever.js
+++ server/quotes-retriever.js
@@ -8,7 +8,8 @@ function QuotesRetriever() {
 
     this.getQuotes = function (fromDate, toDate) {
         var d = q.defer(),
-            promises = [];
+            promises = [],
+            oilPromise = null;
 
         if (fromDate === undefined)
             fromDate = fromDate.today();
@@ -18,18 +19,65 @@ function QuotesRetriever() {
         if (toDate === undefined)
             promises.push(requestQuotes(fromDate));
         else {
-            var cur = fromDate;
+            var cur = fromDate.clone();
             toDate.clearTime();
             while (cur < toDate) {
                 promises.push(requestQuotes(cur.clone()));
                 cur.addDays(1);
             }
         }       
+        oilPromise = requestOilQuotes(fromDate, toDate);
 
-        q.all(promises).then(d.resolve, d.reject);
+        q.all([q.all(promises), oilPromise]).then(function (results) {
+            var quotes = results[0],
+                oilQuotes = results[1];
+            for (var i = 0; i < quotes.length; i++) {
+                var match = oilQuotes.filter(function (e) { return e.date.clearTime().equals(quotes[i].date.clearTime()) })[0];
+                quotes[i].oil = match ? match.high : null;
+            }
+            d.resolve(quotes);
+        }, d.reject);
 
         return d.promise;
     };
+
+    function requestOilQuotes(dateFrom, dateTo) {
+        var d = q.defer(),
+            dateStrFromParam = 'trim_start=' + dateFrom.toFormat('YYYY-MM-DD'),
+            dateStrToParam = dateTo ? '&trim_end=' + dateTo.toFormat('YYYY-MM-DD') : '',
+            url = 'http://www.quandl.com/api/v1/datasets/CHRIS/CME_CL1.json?' + dateStrFromParam + dateStrToParam;
+        request(url, function (error, response, body) {
+            if (error) {
+                console.error(error);
+                d.reject(error);
+            } else if (response.statusCode == 200) {
+                var quotes = parseOilQuotes(body);
+                d.resolve(quotes);
+            }
+        });
+        return d.promise;
+    }
+
+    function parseOilQuotes(responseBody) {
+        var parsedBody = JSON.parse(responseBody),
+            rawQuotes = parsedBody.data,
+            quotes = [];
+        for (var i = 0; i < rawQuotes.length; i++) {
+            var c = rawQuotes[i];
+            quotes.push({
+                date: new Date(c[0]),
+                open: c[1],
+                high: c[2],
+                low: c[3],
+                last: c[4],
+                change: c[5],
+                settle: c[6],
+                volume: c[7],
+                openInterest: c[8],
+            });
+        }
+        return quotes;
+    }
 
     function requestQuotes(date) {
         var d = q.defer(),
